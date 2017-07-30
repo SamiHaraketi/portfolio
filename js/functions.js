a30b9a7f7f6abf147b38cb64d271a4a904957d92
sami haraketi <sami.haraketi@live.fr>
2017-07-30 12:04:48 +0100

    all

--- js/functions.js
+++ js/functions.js
@@ -59,28 +59,38 @@ $(".btn-work-print").click(function(){
 
 
 /*menu*/
-// Get the modal
 var modal = document.getElementById('myModal');
 
-// Get the button that opens the modal
+
 var btn = document.getElementById("btn-menu");
 
-// Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[0];
+var linkPage =  document.getElementsByClassName("link-page");
 
-// When the user clicks the button, open the modal 
+
+var x=0;
+
 btn.onclick = function() {
+    btn.classList.toggle("change");
+    if(x==0)
+    {
     modal.style.display = "block";
-}
-
-// When the user clicks on <span> (x), close the modal
-span.onclick = function() {
-    modal.style.display = "none";
-}
-
-// When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
-    if (event.target == modal) {
-        modal.style.display = "none";
+    x=x+1;
     }
+    else 
+    {
+    modal.style.display = "none";
+    x=x-1;      
+    }
+
+
 }
+
+linkPage.onclick = function() {
+    modal.style.display = "none";
+
+}
+
+
+
+
+
